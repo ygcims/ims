31c2f99b762d3c0fe9e53322acf5670001f764e5
sanuabeysekara <sanuabeysekara@gmail.com>
2023-10-17 22:32:04 +0530

    fourth commit

--- connect.js
+++ connect.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 export const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
+
+
